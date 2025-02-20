f75fb0c700b26283b91ca36332dcfaa2622e770a
Mehmet Eren Efe <erenefe1382@gmail.com>
2025-02-21 01:04:32 +0300

    Create register controller

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class RegisterController extends Controller
+{
+    public function show(){
+        return view('auth.register');
+    }
+
+    public function register(Request $request){
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:8|confirmed',
+        ]);
+
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return redirect()->route('login')->with('success', 'Kayıt Başarılı, giriş yapabilirsiniz.');
+    }
+}
