c13c4a8078e945590e0233b955b0226ddc0e9032
Mehmet Eren Efe <erenefe1382@gmail.com>
2025-02-21 17:21:07 +0300

    Create dasboard page & fix login controller

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -14,7 +14,7 @@ class LoginController extends Controller
     public function login(Request $request){
         $request->validate([
             'email' => ['required', 'email'],
-            'password' => ['password'],
+            'password' => ['required'],
         ]);
 
         if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,1 @@
+<h1>Hoş geldiniz, {{ auth()->user()->name }}!</h1>
