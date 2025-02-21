807ab2589f9b667979ae827e3aec021a880ee6a3
Mehmet Eren Efe <erenefe1382@gmail.com>
2025-02-21 16:27:58 +0300

    Create login controller

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function show(){
+        return view('auth.login');
+    }
+
+    public function login(Request $request){
+        $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['password'],
+        ]);
+
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            return redirect()->route('dashboard')->with('success', 'Giriş Başarılı');
+        }
+
+        return back()->withErrors(['email', 'Giriş Bilgileri Hatalı!']);
+    }
+
+    public function logout(){
+        Auth::logout();
+        return redirect()->route('login')->with('success', 'Çıkış Yaptınız');
+    }
+}
